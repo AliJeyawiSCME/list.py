8e9514c516639441b0275c320ed078d07918f43d
AliJeyawiSCME <22310023@scme.edu.ps>
2024-11-03 09:46:19 +0200

    Create list.py

---
+++ list.py
@@ -0,0 +1,23 @@
+list1 = [2,3]
+print("list =", list1)
+print("list length =",len(list1))
+print("list maximum number =", max(list1))
+print("list minimum number =", min(list1))
+print("list sum =",sum(list1))
+#import random
+#random.shuffle(list1)
+#print("shuffled list =", list1)
+
+list2 = [1,9]
+list3 = list1 + list2
+print("list3 =",list3)
+list3 = 3 * list1
+print("2 * list1 =",list3)
+list4 = list3[2:4]
+print("list3 2:4 =",list4)
+
+print("")
+list5 = [2,3,4,2,33,21]
+print(list5[-1])
+print(list5[-3])
+
