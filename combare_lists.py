3ab69e074f4296da937d0d3b2d1e5b6ffb14b0f6
AliJeyawiSCME <22310023@scme.edu.ps>
2024-11-05 08:44:00 +0200

    Create combare_lists.py

---
+++ combare_lists.py
@@ -0,0 +1,9 @@
+list1 = ["green", "red", "blue"]
+list2 = ["red", "blue", "green"]
+
+print("list2 == list1:", list2 == list1)
+print("list2 != list1:", list2 != list1)
+print("list2 >= list1:", list2 >= list1)
+print("list2 > list1:", list2 > list1)
+print("list2 < list1:", list2 < list1)
+print("list2 <= list1:", list2 <= list1)
