0dac431d608bf783e57ccf8e86e0395bd0bb2ae2
AliJeyawiSCME <22310023@scme.edu.ps>
2024-11-17 09:27:59 +0200

    .

--- lists/matrix.py
+++ lists/matrix.py
@@ -8,3 +8,15 @@ for row in range(0, numberOfRows):
     for columns in range(0, numberOfColumns):
         matrix[row].append(random.randrange(0, 100))
 print(matrix)
+
+# total = 0
+# for row in range(0, len(matrix)):
+#     for column in range(0, len(matrix[row])):
+#         total += matrix[row][column]
+# print("Total is " + str(total))
+
+total = 0
+for column in range(0, len(matrix[0])):
+    for row in range(0, len(matrix)):
+        total += matrix[row][column]
+    print("Column sum " + str(column), "is " + str(total))
