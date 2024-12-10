6a316ac18ef0451691ba9b76d4be868f453d33bd
AliJeyawiSCME <22310023@scme.edu.ps>
2024-12-10 09:26:03 +0200

    Update linkedList.py

--- lists/linkedList.py
+++ lists/linkedList.py
@@ -13,7 +13,32 @@ class LinkedList:
         if self.__tail == None:
             self.__tail = self.__head
 
+    def addLast(self, e):
+        newNode = Node(e)
 
+        if self.__tail == None:
+            self.__head = self.__tail = newNode
+        else:
+            self.__tail.next = newNode
+            self.__tail = self.__tail.next 
+
+
+    def insert(self, index, e):
+        if index == 0:
+            self.addFirst(e)
+        elif index >= self.__size:
+            self.addLast(e)
+        else:
+            current = self.__head
+            for i in range(1, index):
+                current = current.next
+            temp = current.next
+            current.next = Node(e)
+            (current.next).next = temp
+            self.__size += 1
+
+    def add(self, e):
+        self.addLast(e)
 
 
 
